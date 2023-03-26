aedc1148c8bc76a92a4037daddf55e43b1b30cd4
LucianoDLima <ljr.luciano.junior@outlook.com>
2023-03-26 14:20:10 -0300

    search bar added, static only, not responsive yet

---
+++ src/components/SearchFilterContainer/SearchFilterContainer.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import { ThemeContext } from '../../context/ThemeContext'
+import { Filter } from './Filter/Filter'
+import SearchBar from './SearchBar/SearchBar'
+
+const SearchFilterContainer = () => {
+  const { theme } = useContext(ThemeContext)
+  return (
+    <section className={`search-filter-container ${theme ? '' : 'light-mode'}`}>
+      <SearchBar />
+      {/* <Filter /> */}
+    </section>
+  )
+}
+
+export default SearchFilterContainer
--- src/pages/Base/Base.jsx
+++ src/pages/Base/Base.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ThemeProvider } from '../../context/ThemeContext'
+import SearchFilterContainer from '../../components/SearchFilterContainer/SearchFilterContainer'
 import Header from '../Header/Header'
 
 const Base =  () => {
   return (
     <ThemeProvider>
       <Header />
+      <SearchFilterContainer />
     </ThemeProvider>
   )
 }
