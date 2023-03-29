720a316ed2e2a4da0511c5f1f87e008b5d9b6f24
LucianoDLima <ljr.luciano.junior@outlook.com>
2023-03-28 21:59:31 -0300

    worked a bit on the individual pages when you click a country for more info

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Button = ({ text, image, to }) => {
+const Button = ({ text, image, to, className }) => {
   return (
     <Link to={to}>
-      <button className="main-button">
+      <button className={`main-button ${className}`}>
       {/* The style was loading even if no image, which was causing issues, which is why the condition */}
       {image && <img src={image}/>}
         {text}
--- src/components/CountryLink/CountryLink.jsx
+++ src/components/CountryLink/CountryLink.jsx
@@ -7,7 +7,7 @@ import arrowDark from '../../images/pointer-arrow-dark.svg';
 import { ThemeContext } from '../../context/ThemeContext';
 
 const CountryLink = () => {
-  const { theme } = useContext(ThemeContext)
+  const { theme } = useContext(ThemeContext);
   const param = useParams();
   const [countryData, setCountryData] = useState({});
   const [loading, setLoading] = useState(true);
@@ -25,7 +25,7 @@ const CountryLink = () => {
         }
 
         const data = await response.json();
-
+        console.log(data);
         setCountryData(data[0]);
         setLoading(false);
         setError404(false);
@@ -39,14 +39,61 @@ const CountryLink = () => {
   }, [param.id]);
 
   return (
-    <section className='country-detail'>
+    <section className="country-detail">
       {error404 ? (
         <Error />
       ) : loading ? (
         <p>Loading...</p>
       ) : (
-        <div>
-          <Button image={theme ? arrowDark : arrowLight} text='Back' to='/'/>
+        <div className="country-detail__container">
+          <Button
+            image={theme ? arrowDark : arrowLight}
+            text="Back"
+            to="/"
+            className="back-button"
+          />
+          <div className="country-detail__information">
+            <div className="country-detail__image-box">
+              <img src={countryData.flags.svg} />
+            </div>
+
+            <div className="country-detail__information-content">
+              <h2>{countryData.name.common}</h2>
+
+              <ul className="country-detail__general">
+                {[
+                  {
+                    label: 'Native Name',
+                    value:
+                      countryData.name.nativeName[
+                        Object.keys(countryData.name.nativeName)[0]
+                      ].common,
+                  },
+                  {
+                    label: 'Population',
+                    value: countryData.population
+                      .toLocaleString()
+                      .replace(/,/g, '.'),
+                  },
+                  { label: 'Region', value: countryData.region },
+                  { label: 'Sub Region', value: countryData.subregion },
+                  { label: 'Capital', value: countryData.capital},
+                  { label: 'Top Level Domain', value: countryData.tld},
+                  { label: 'Currencies', value: Object.keys(countryData.currencies)
+                  .map((currency) => countryData.currencies[currency].name)
+                  .join(", ")},
+                  { label: 'Language', value: Object.keys(countryData.languages).map(lang => countryData.languages[lang]).join(", ")},
+                  { label: 'Driving side', value: countryData.car.side}
+                ].map(({ label, value }) => (
+                  <li key={label}>
+                    <p>
+                      {label}: <span>{value}</span>
+                    </p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       )}
     </section>
