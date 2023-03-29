a833b231d4e34c7b0a12b0152d3b5c59f4696d8f
LucianoDLima <ljr.luciano.junior@outlook.com>
2023-03-29 07:52:15 -0300

    added the border buttons and finally figured out how to make em work

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Button = ({ text, image, to, className }) => {
+const Button = ({ text, image, to, className, click }) => {
   return (
     <Link to={to}>
-      <button className={`main-button ${className}`}>
+      <button className={`main-button ${className}`} onClick={click}>
       {/* The style was loading even if no image, which was causing issues, which is why the condition */}
       {image && <img src={image}/>}
         {text}
--- src/components/CountryLink/CountryLink.jsx
+++ src/components/CountryLink/CountryLink.jsx
@@ -9,40 +9,69 @@ import { ThemeContext } from '../../context/ThemeContext';
 const CountryLink = () => {
   const { theme } = useContext(ThemeContext);
   const param = useParams();
-  const [countryData, setCountryData] = useState({});
+  
   const [loading, setLoading] = useState(true);
+  const [countryData, setCountryData] = useState([]);
+  const [borderCountries, setBorderCountries] = useState([]);
   const [error404, setError404] = useState(false);
 
+  console.log('border:', borderCountries);
+
+
+  // This will fetch the data of the previously clicked country on the main page plus its borders if it has one
   useEffect(() => {
-    const fetchCountryName = async () => {
+    const fetchCountryData = async () => {
+      // Previously clicked country
       try {
         const response = await fetch(
-          `https://restcountries.com/v3.1/name/${param.id}/?fullText=true`
+          `https://restcountries.com/v3.1/name/${param.id}?fullText=true`
         );
-
         if (!response.ok) {
           throw new Error(response.status);
         }
-
         const data = await response.json();
-        console.log(data);
+        
         setCountryData(data[0]);
-        setLoading(false);
-        setError404(false);
+
+        // checks if the country has borders
+        const borders = data[0]?.borders;
+        // if country has at least one border, the next API is called
+        if (borders && borders.length > 0) {
+          const borderPromises = borders.map((border) =>
+            fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+          );
+          const borderResponses = await Promise.all(borderPromises);
+
+          const borderDataPromises = borderResponses.map((response) =>
+            response.json()
+          );
+          const borderData = await Promise.all(borderDataPromises);
+          setLoading(false);
+          setError404(false);
+          console.log(borderData);
+          setBorderCountries(borderData);
+        } else {
+          setLoading(false);
+          setError404(false);
+        }
       } catch (error) {
         setError404(true);
         setLoading(false);
       }
     };
-    fetchCountryName();
+
+    fetchCountryData();
   }, [param.id]);
 
   return (
     <section className="country-detail">
-      {error404 ? (
-        <Error />
-      ) : loading ? (
+      {/* It checks if loading is still true,
+      then if there's an error after API is called,
+      and only then it will render the page with an error or the content*/}
+      {loading ? (
         <p>Loading...</p>
+        ) : error404 ? (
+        <Error />
       ) : (
         <div className="country-detail__container">
           <Button
@@ -50,6 +79,7 @@ const CountryLink = () => {
             text="Back"
             to="/"
             className="back-button"
+            click={() => setLoading(true)}
           />
           <div className="country-detail__information">
             <div className="country-detail__image-box">
@@ -76,13 +106,21 @@ const CountryLink = () => {
                   },
                   { label: 'Region', value: countryData.region },
                   { label: 'Sub Region', value: countryData.subregion },
-                  { label: 'Capital', value: countryData.capital},
-                  { label: 'Top Level Domain', value: countryData.tld},
-                  { label: 'Currencies', value: Object.keys(countryData.currencies)
-                  .map((currency) => countryData.currencies[currency].name)
-                  .join(", ")},
-                  { label: 'Language', value: Object.keys(countryData.languages).map(lang => countryData.languages[lang]).join(", ")},
-                  { label: 'Driving side', value: countryData.car.side}
+                  { label: 'Capital', value: countryData.capital },
+                  { label: 'Top Level Domain', value: countryData.tld },
+                  {
+                    label: 'Currencies',
+                    value: Object.keys(countryData.currencies)
+                      .map((currency) => countryData.currencies[currency].name)
+                      .join(', '),
+                  },
+                  {
+                    label: 'Language',
+                    value: Object.keys(countryData.languages)
+                      .map((lang) => countryData.languages[lang])
+                      .join(', '),
+                  },
+                  { label: 'Driving side', value: countryData.car.side },
                 ].map(({ label, value }) => (
                   <li key={label}>
                     <p>
@@ -91,6 +129,21 @@ const CountryLink = () => {
                   </li>
                 ))}
               </ul>
+
+              <div className="country-details__borders">
+                <p>Border Countries:</p>
+                <ul>
+                  {borderCountries.length === 0 ? 'No border countries' : Object.keys(countryData.borders).map((border, index) => (
+                    <li key={countryData.borders[border]}>
+                      <Button
+                        text={countryData.borders[border]}
+                        to={`../${borderCountries[index][0].name.common}`}
+                        click={() => setLoading(true)}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
