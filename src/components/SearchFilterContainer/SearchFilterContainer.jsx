62b101366eae5369d9f18366b4a767365995f65e
LucianoDLima <ljr.luciano.junior@outlook.com>
2023-03-30 23:28:57 -0300

    fixed an issue where the last letter deleted in input would not render all countries back

--- src/components/SearchFilterContainer/SearchFilterContainer.jsx
+++ src/components/SearchFilterContainer/SearchFilterContainer.jsx
@@ -3,35 +3,11 @@ import { ThemeContext } from '../../context/ThemeContext';
 import { Filter } from '../Filter/Filter';
 import SearchBar from '../SearchBar/SearchBar';
 
-const SearchFilterContainer = ({value}) => {
-  const { theme } = useContext(ThemeContext);
-  const [searchInput, setSearchInput] = useState('');
-  const [countryData, setCountryData] = useState([]);
-  useEffect(() => {
-    const fetchCountryData = async () => {
-      try {
-        const response = await fetch(
-          `https://restcountries.com/v3.1/name/${searchInput}`
-        );
-
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-
-        const data = await response.json();
-        console.log(data);
-        setCountryData(data);
-      } catch (error) {
-        console.log('catch:', error);
-      }
-    };
-
-    fetchCountryData();
-  }, [searchInput]);
+const SearchFilterContainer = ({value, onChange}) => {
 
   return (
     <section className={`search-filter-container`}>
-      <SearchBar value={value} />
+      <SearchBar value={value} onChange={onChange}/>
       <Filter />
     </section>
   );
--- src/pages/Base/Base.jsx
+++ src/pages/Base/Base.jsx
@@ -5,41 +5,75 @@ import Main from '../Main/Main';
 
 const Base = () => {
   const [countryData, setCountryData] = useState([]);
+  const [allCountries, setAllCountries] = useState(true);
+  const [searchInput, setSearchInput] = useState('');
 
   // Fetches all the countries
   useEffect(() => {
-    const fetchCountryData = async () => {
-      try {
-        const response = await fetch('https://restcountries.com/v3.1/all');
-        const data = await response.json();
-        setCountryData(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+    // The if is necessary so the API is called again when you delete the text inside the search input
+    if (allCountries) {
+      setAllCountries(false)
+      const fetchCountryData = async () => {
+        try {
+          const response = await fetch('https://restcountries.com/v3.1/all');
+          const data = await response.json();
+          setCountryData(data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
 
-    fetchCountryData();
-  }, []);
+      fetchCountryData();
+    }
+  }, [allCountries]);
+
+  useEffect(() => {
+    if (searchInput) {
+      const fetchCountryData = async () => {
+        try {
+          const response = await fetch(
+            `https://restcountries.com/v3.1/name/${searchInput}`
+          );
+
+          if (!response.ok) {
+            throw new Error(response.status);
+          }
+
+          const data = await response.json();
+          console.log(data);
+          setCountryData(data);
+        } catch (error) {
+          console.log('catch:', error);
+        }
+      };
+
+      fetchCountryData();
+      setAllCountries(false);
+    } else {
+      setAllCountries(true);
+    }
+  }, [searchInput]);
+
+  const inputSearch = (e) => {
+    setSearchInput(e.target.value);
+  };
 
   return (
     <>
-      <SearchFilterContainer />
+      <SearchFilterContainer onChange={inputSearch} value={searchInput} />
       <main>
-      {countryData.map((country) => (
-        <CountryCards
-          key={country.name.common}
-          flag={country.flags.svg}
-          flagAlt={country.flags.alt}
-          countryName={country.name.common}
-          population={country.population
-            .toLocaleString()
-            .replace(/,/g, '.')}
-          region={country.region}
-          capital={country.capital}
-          to={country.name.common}
-        />
-        
-      ))}
+        {countryData.map((country) => (
+          <CountryCards
+            key={country.name.common}
+            flag={country.flags.svg}
+            flagAlt={country.flags.alt}
+            countryName={country.name.common}
+            population={country.population.toLocaleString().replace(/,/g, '.')}
+            region={country.region}
+            capital={country.capital}
+            to={country.name.common}
+          />
+        ))}
       </main>
     </>
   );
